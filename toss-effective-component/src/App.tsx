598b6d31293c960a4916695056dd7db83eb999e3
kimseunggyu <dev.seunggyu@gmail.com>
2023-10-27 21:33:28 +0900

    refactor: toss-effective-component example1 불필요한 코드 제거

--- toss-effective-component/src/App.tsx
+++ toss-effective-component/src/App.tsx
@@ -1,6 +1,6 @@
-// import { ExampleApp1 } from './example1/App.tsx';
+import { ExampleApp1 } from './example1/App.tsx';
 // import { ExampleApp2 } from '@/example2/App.tsx';
-import { ExampleApp3 } from '@/example3/App.tsx';
+// import { ExampleApp3 } from '@/example3/App.tsx';
 // import { ExampleApp4 } from '@/example4/App.tsx';
 // import { ExampleApp5 } from '@/example5/App.tsx';
 // import { ExampleApp6 } from '@/example6/App.tsx';
@@ -8,9 +8,9 @@ import { ExampleApp3 } from '@/example3/App.tsx';
 function App() {
   return (
     <>
-      {/*<ExampleApp1 />*/}
+      <ExampleApp1 />
       {/*<ExampleApp2 />*/}
-      <ExampleApp3 />
+      {/*<ExampleApp3 />*/}
       {/*<ExampleApp4 />*/}
       {/*<ExampleApp5 />*/}
       {/*<ExampleApp6 />*/}
